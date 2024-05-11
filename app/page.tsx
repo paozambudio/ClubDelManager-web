d5cc6c2b628c5c7858d5e2e686ef5ab7cb666a88
Pao Zambudio <paozambudio@MacBook-Pro-de-Pao.local>
2024-05-11 14:55:34 -0300

    Cambie page para probar

--- app/page.tsx
+++ app/page.tsx
@@ -11,11 +11,13 @@ const HomePage = async () => {
   return (
     <>
       <Banner altText="Imagen de banner" />
-
+      <div>
+        <h3>estoy en dev</h3>
+      </div>
       <div className="mx-auto max-w-5xl px-6 pt-6 flex flex-col justify-center items-center">
         <NumbersHome
           miembrosCount={70}
-          proyectosCount={5}
+          proyectosCount={15}
           participacionCount={15}
         />
         <GetNoticias />
