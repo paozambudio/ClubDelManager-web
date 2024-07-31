25afe91e9780f44273b2034a4c4bfb21c769333e
Pao Zambudio <pao.zambudio@gmail.com>
2024-07-31 20:18:17 -0300

    Agregué filtro de beneficios

--- components/home/BeneficiosCDM.jsx
+++ components/home/BeneficiosCDM.jsx
@@ -26,16 +26,23 @@ const BeneficiosCDM = () => {
     },
   ]);
   const [tipos, setTipos] = useState([]);
-  const [selectedTipo, setSelectedTipo] = useState();
-
-  // Estado para manejar la paginación
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 4; // Número de tarjetas por página
+  const [selectedTipo, setSelectedTipo] = useState("");
 
   useEffect(() => {
     fetchData(`${apiDomain}/members/beneficios/`);
   }, []);
 
+  useEffect(() => {
+    if (selectedTipo) {
+      console.log("Tipo de beneficio seleccionado: ", selectedTipo);
+      if (selectedTipo != "Todos")
+        fetchData(
+          `${apiDomain}/members/beneficios/by_tipo/?tipo=${selectedTipo}`
+        );
+      else fetchData(`${apiDomain}/members/beneficios/`);
+      // Aquí puedes realizar alguna acción con el tipo seleccionado, como filtrar beneficios
+    }
+  }, [selectedTipo]);
   const fetchData = async (url) => {
     setLoading(true);
     try {
@@ -45,10 +52,14 @@ const BeneficiosCDM = () => {
 
       const data = response.data.results;
       setBeneficiosData(data);
+      console.log("Beneficios:", data);
 
-      const tipos_beneficio = Array.from(
-        new Set(data.map((bene) => bene.tipo_beneficio))
-      );
+      //tipo-beneficio
+
+      const responseTipos = await fetch(`${apiDomain}/members/tipo-beneficio/`);
+      const tipos_beneficio = await responseTipos.json();
+      console.log("Tipos bene:", tipos_beneficio);
+
       setTipos(tipos_beneficio);
     } catch (error) {
       console.error("Error fetching data:", error);
@@ -70,39 +81,43 @@ const BeneficiosCDM = () => {
     }
   };
 
-  const handleChangeTipoBeneficio = () => {};
+  const handleChangeTipoBeneficio = (e) => {
+    setSelectedTipo(e.target.value);
+    console.log("Tipo seleccionado: ", selectedTipo);
+  };
 
   return (
     <div className="transparent">
       {!session && (
         <div className="container px-6 py-2 mx-auto justify-items-center flex-col">
-          <span className="cols cols-2">
-            <h1 class="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-gray-800">
-              Beneficios <br /> de los miembros&nbsp;
-              <span class="underline decoration-blue-500">Del Club</span>
-            </h1>
-            {/* <div>
-              <label className="block text-gray-700 font-semibold mb-2">
-                Seleccione categoría
-              </label>
+          <div className="container py-10 pt-10 align-top mx-auto mt-6 md:flex md:items-center md:justify-between ">
+            <span className="cols cols-2">
+              <h1 class="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-gray-800">
+                Beneficios <br /> de los miembros&nbsp;
+                <span class="underline decoration-blue-500">Del Club</span>
+              </h1>
+            </span>
+            <br />
+            <div className="w-80">
               <select
                 type="text"
                 id=""
                 name=""
-                className="border rounded w-full py-2 px-3 mb-2 bg-gray-100"
+                className="border rounded w-full py-2 px-3 mb-2 bg-gray-200 text-gray-400 font-sans text-md"
                 value={selectedTipo}
                 onChange={handleChangeTipoBeneficio}
               >
-                <option value="Otra">(Todas)</option>
-                {tipos.map((uno) => (
-                  <option value={uno.tipo_beneficio}>
-                    {uno.tipo_beneficio}
+                <option key="Todos" value="Todos">
+                  (Ver todos)
+                </option>
+                {tipos.map((tipo, index) => (
+                  <option key={index} value={tipo}>
+                    {tipo}
                   </option>
                 ))}
               </select>
-            </div> */}
-          </span>
-
+            </div>
+          </div>
           <div className="grid grid-cols-1 gap-4 mt-6 xl:mt-12 xl:gap-2 md:grid-cols-2 lg:grid-cols-4 justify-start">
             {beneficiosData.map((bene) => (
               <div
