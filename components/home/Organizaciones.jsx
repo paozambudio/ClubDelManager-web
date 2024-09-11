afaabe4e458944f53002187976901c47e7efe3e4
Pao Zambudio <pao.zambudio@gmail.com>
2024-09-11 18:17:59 -0300

    Muestro organizaciones solo si no hay sesi´n

--- components/home/Organizaciones.jsx
+++ components/home/Organizaciones.jsx
@@ -1,52 +1,62 @@
-const BeneficiosCDM = () => {
+"use client";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+
+const Organizaciones = () => {
+  const { data: session } = useSession();
+
   return (
-    <div>
-      <div className="container  pt-10 align-top mx-auto mt-6 md:flex md:items-center md:justify-between ">
-        <span className="cols cols-2">
-          <h1 class="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-gray-800">
-            Organizaciones que confían en <br />
-            <span class="underline decoration-blue-500">
-              El Club Del Manager
-            </span>
-          </h1>
-        </span>
-      </div>
-      <div className="grid grid-cols-1 gap-4 mt-6 xl:mt-12 xl:gap-2 md:grid-cols-2 lg:grid-cols-4 justify-start">
-        <div className="w-full text-center rounded-lg flex flex-col relative group">
-          <div className="flex flex-col items-center justify-between flex-grow">
-            <div className="flex items-center justify-center mt-4">
-              <img
-                src="/img-logo-organizaciones/logo-iuce.png"
-                className="h-40 w-40 object-contain"
-                alt="Logo"
-              />
+    <div className="transparent">
+      {!session && (
+        <div className="container  pt-10 align-top mx-auto mt-6 md:flex md:items-center md:justify-between ">
+          <span className="cols cols-2">
+            <h1 class="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-gray-800">
+              Organizaciones que confían en <br />
+              <span class="underline decoration-blue-500">
+                El Club Del Manager
+              </span>
+            </h1>
+          </span>
+        </div>
+      )}
+      {!session && (
+        <div className="grid grid-cols-1 gap-4 mt-6 xl:mt-12 xl:gap-2 md:grid-cols-2 lg:grid-cols-4 justify-start">
+          <div className="w-full text-center rounded-lg flex flex-col relative group">
+            <div className="flex flex-col items-center justify-between flex-grow">
+              <div className="flex items-center justify-center mt-4">
+                <img
+                  src="/img-logo-organizaciones/logo-iuce.png"
+                  className="h-40 w-40 object-contain"
+                  alt="Logo"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="w-full  text-center rounded-lg flex flex-col relative group">
+            <div className="flex flex-col items-center justify-between flex-grow">
+              <div className="flex items-center justify-center mt-4">
+                <img
+                  src="/img-logo-organizaciones/uca-logo.webp"
+                  className="h-40 w-40 object-contain"
+                  alt="Logo"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="w-full text-center rounded-lg flex flex-col relative group">
+            <div className="flex flex-col items-center justify-between flex-grow">
+              <div className="flex items-center justify-center mt-4">
+                <img
+                  src="/img-logo-organizaciones/unlar_logo.png"
+                  className="h-40 w-40 object-contain"
+                  alt="UNLaR"
+                />
+              </div>
             </div>
           </div>
         </div>
-        <div className="w-full  text-center rounded-lg flex flex-col relative group">
-          <div className="flex flex-col items-center justify-between flex-grow">
-            <div className="flex items-center justify-center mt-4">
-              <img
-                src="/img-logo-organizaciones/uca-logo.webp"
-                className="h-40 w-40 object-contain"
-                alt="Logo"
-              />
-            </div>
-          </div>
-        </div>
-        <div className="w-full text-center rounded-lg flex flex-col relative group">
-          <div className="flex flex-col items-center justify-between flex-grow">
-            <div className="flex items-center justify-center mt-4">
-              <img
-                src="/img-logo-organizaciones/unlar_logo.png"
-                className="h-40 w-40 object-contain"
-                alt="UNLaR"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
-export default BeneficiosCDM;
+export default Organizaciones;
