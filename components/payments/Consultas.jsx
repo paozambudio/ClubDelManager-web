035d1b842f849dbc6a7d323e87da28014f0eeab1
Pao Zambudio <pao.zambudio@gmail.com>
2024-09-26 15:31:33 -0300

    Responsive

--- components/payments/Consultas.jsx
+++ components/payments/Consultas.jsx
@@ -10,28 +10,19 @@ const ConsultasPagos = () => {
     return { mes, anio };
   };
 
-  // Estado para almacenar el mes y el año
   const [mes, setMes] = useState(obtenerMesAnioActual().mes);
   const [anio, setAnio] = useState(obtenerMesAnioActual().anio);
-  const [data, setData] = useState(null); // Para almacenar los datos de la respuesta
-  const [error, setError] = useState(null); // Para almacenar cualquier error
-  const [filter, setFilter] = useState("all"); // Estado del filtro ('all', 'paid', 'unpaid')
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [filter, setFilter] = useState("all");
 
-  // Manejar cambios en el combo del mes
-  const handleMesChange = (e) => {
-    setMes(e.target.value);
-  };
-
-  // Manejar cambios en el campo del año
-  const handleAnioChange = (e) => {
-    setAnio(e.target.value);
-  };
+  const handleMesChange = (e) => setMes(e.target.value);
+  const handleAnioChange = (e) => setAnio(e.target.value);
 
   const [membersStatus, setMembersStatus] = useState([]);
   const [paidCount, setPaidCount] = useState(0);
   const [unpaidCount, setUnpaidCount] = useState(0);
 
-  // Función para consultar los pagos
   const consultarPagos = async (mes, anio) => {
     const periodo = `${mes}-${anio}`;
     console.log("Consultando pagos para el período:", periodo);
@@ -43,10 +34,6 @@ const ConsultasPagos = () => {
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
-      /* const result = await response.json();
-      console.log("Resultado: ", result);
-      setData(result); */
-
       const data = await response.json();
       setMembersStatus(data.members_status);
       setPaidCount(data.paid_count);
@@ -56,35 +43,29 @@ const ConsultasPagos = () => {
     }
   };
 
-  // useEffect para realizar la consulta cuando cambian mes o anio
   useEffect(() => {
     consultarPagos(mes, anio);
-  }, [mes, anio]); // Dependencias
+  }, [mes, anio]);
 
-  // Filtrar la lista de miembros según el filtro seleccionado
   const filteredMembers = membersStatus.filter((member) => {
     if (filter === "paid") return member.has_paid;
     if (filter === "unpaid") return !member.has_paid;
-    return true; // 'all'
+    return true;
   });
 
   return (
-    <div className="container px-6 py-10 mx-auto">
-      <h1 className="text-xl font-semibold text-sky-800 lg:text-3xl">
+    <div className="container px-4 py-6 mx-auto">
+      <h1 className="text-lg font-semibold text-sky-800 lg:text-3xl">
         Pago de membresía
       </h1>
       <br />
-      <h2 className="  text-gray-600 px-3 py-2">
-        Seleccione el período a consultar
-      </h2>
-      <br />
+      <h2 className="text-gray-600">Seleccione el período a consultar</h2>
 
-      <div className="flex items-center gap-4">
-        {/* Combo para el mes */}
+      <div className="flex flex-col md:flex-row items-center gap-4 mb-4">
         <select
           value={mes}
           onChange={handleMesChange}
-          className="border bg-gray-100 text-gray-500 px-3 py-2 rounded-lg"
+          className="border bg-gray-100 text-gray-500 px-3 py-2 rounded-lg w-full md:w-auto"
         >
           <option value="01">Enero</option>
           <option value="02">Febrero</option>
@@ -100,114 +81,85 @@ const ConsultasPagos = () => {
           <option value="12">Diciembre</option>
         </select>
 
-        {/* Campo de texto para el año */}
         <input
           type="number"
           value={anio}
           onChange={handleAnioChange}
-          className="border bg-gray-100 text-gray-500 px-3 py-2 rounded-lg"
+          className="border bg-gray-100 text-gray-500 px-3 py-2 rounded-lg w-full md:w-auto"
           placeholder="aaaa"
         />
-        <div>
+
+        <div className="text-gray-600">
           <div>
-            <span className="  text-gray-600 px-3 py-2">
-              Pagaron cuota: &nbsp;&nbsp;<b>{paidCount}</b>
-            </span>
+            Pagaron cuota: <b>{paidCount}</b>
           </div>
           <div>
-            <span className="  text-gray-600 px-3 py-2">
-              Adeudan cuota: &nbsp;&nbsp;<b>{unpaidCount}</b>
-            </span>
+            Adeudan cuota: <b>{unpaidCount}</b>
           </div>
         </div>
       </div>
 
-      <br />
-
-      {/* Grupo de Radio Buttons para el filtro */}
-      <div style={styles.radioFrame}>
-        <label style={styles.radioLabel}>
+      {/* Grupo de Radio Buttons */}
+      <div className="p-4 border rounded-lg bg-gray-100 mb-4">
+        <label className="mr-4">
           <input
             type="radio"
             name="paymentFilter"
             value="all"
             checked={filter === "all"}
             onChange={() => setFilter("all")}
-            style={styles.radioInput}
+            className="mr-2"
           />
           Todos
         </label>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <label style={styles.radioLabel}>
+        <label className="mr-4">
           <input
             type="radio"
             name="paymentFilter"
             value="paid"
             checked={filter === "paid"}
             onChange={() => setFilter("paid")}
-            style={styles.radioInput}
+            className="mr-2"
           />
-          Solo los que cancelaron
+          Solo los que pagaron
         </label>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <label style={styles.radioLabel}>
+        <label>
           <input
             type="radio"
             name="paymentFilter"
             value="unpaid"
             checked={filter === "unpaid"}
             onChange={() => setFilter("unpaid")}
-            style={styles.radioInput}
+            className="mr-2"
           />
           Solo los pendientes
         </label>
       </div>
-      <br />
-      <div>
-        <br />
-        <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-          <thead class="bg-gray-100">
-            <tr>
-              <th
-                scope="col"
-                class="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500"
-              >
-                Nombre
-              </th>
 
-              <th
-                scope="col"
-                class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500"
-              >
-                Apellido
-              </th>
-              <th
-                scope="col"
-                class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500"
-              >
-                Estado
-              </th>
+      <div className="overflow-x-auto">
+        <table className="min-w-full divide-y divide-gray-200">
+          <thead className="bg-gray-100">
+            <tr>
+              <th className="px-4 py-2 text-left text-gray-500">Nombre</th>
+              <th className="px-4 py-2 text-left text-gray-500">Apellido</th>
+              <th className="px-4 py-2 text-left text-gray-500">Estado</th>
             </tr>
           </thead>
-          <tbody class="bg-silver-800 text-gray-600 divide-y divide-gray-200">
+          <tbody className="divide-y divide-gray-200 text-gray-600">
             {filteredMembers.map(
               ({ first_name, last_name, has_paid }, index) => (
-                <tr>
-                  <td class="px-4 py-4 text-sm font-medium whitespace-nowrap">
-                    {first_name}
-                  </td>
-                  <td class="px-4 py-4 text-sm font-medium whitespace-nowrap">
-                    {last_name}
-                  </td>
-                  <td class="px-4 py-4 text-sm font-medium whitespace-nowrap">
+                <tr key={index}>
+                  <td className="px-4 py-2">{first_name}</td>
+                  <td className="px-4 py-2">{last_name}</td>
+                  <td className="px-4 py-2">
                     {has_paid ? (
-                      <div class="inline px-3 py-1 text-sm font-normal rounded-full text-emerald-500 gap-x-2 bg-emerald-100/60 dark:bg-gray-800">
+                      <span className="px-2 py-1 text-sm text-emerald-600 bg-emerald-100 rounded-full">
                         Cancelado
-                      </div>
+                      </span>
                     ) : (
-                      <div class="inline px-3 py-1 text-sm font-normal rounded-full text-red-500 gap-x-2 bg-red-100/60 dark:bg-gray-800">
+                      <span className="px-2 py-1 text-sm text-red-600 bg-red-100 rounded-full">
                         Pendiente
-                      </div>
+                      </span>
                     )}
                   </td>
                 </tr>
@@ -216,32 +168,10 @@ const ConsultasPagos = () => {
           </tbody>
         </table>
       </div>
-      {/* Mostrar datos o error */}
-      {/*  {data && <div>{JSON.stringify(data)}</div>} */}
 
-      {/*no pagaron*/}
-      {/*{data.unpaid_members.map(({ first_name, last_name }, index) => ())}*/}
       {error && <div>Error: {error}</div>}
     </div>
   );
 };
 
 export default ConsultasPagos;
-
-const styles = {
-  radioFrame: {
-    padding: "10px",
-    borderRadius: "10px",
-    border: "1px solid #f9f9f9",
-
-    display: "inline-block",
-    marginBottom: "20px",
-  },
-  radioLabel: {
-    marginRight: "20px",
-    fontSize: "16px",
-  },
-  radioInput: {
-    marginRight: "8px",
-  },
-};
